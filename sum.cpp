2a83987fa5c2356362c2d746a5a6b12be5d8332e
Nkubito Pacis <pacisnkubito@gmail.com>
2021-10-14 14:24:45 +0200

    inital commit

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter two numbers..."<<endl;
+    cout<<"First Number:";
+    cin>>a;
+    cout<<"Second Number:";
+    cin>>b;
+    int c = a + b;
+    cout<<"Addition of "<<a<<"and "<<b<<" is: "<<c<<endl;
+    return 0;
+}
