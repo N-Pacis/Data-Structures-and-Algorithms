b5a8fe5ce0bb453ec6dc61930bae573af8af765d
Nkubito Pacis <pacisnkubito@gmail.com>
2021-11-04 13:55:07 +0200

    Added Binary Additon Program

---
+++ BinaryAdditionDemo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    short a = 0b01000011;
+    short b = 0b00010111;
+    short sum = a + b;
+    cout << "Sum of "<<a<<" and "<<b<<" is: "<<sum<<endl;
+}
