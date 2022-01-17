b5fdc6160fd96a4c49dde9ef55f3eb6c8c58dd2b
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-17 18:42:11 +0200

    [chore] working on point of equilibrium program

---
+++ pointOfEquilibrium.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+using namespace std;
+
+int pointOfEquilibrium(int* arr,int n){
+    int currentElement = 1;
+    for(int i=1;i<n;i++){
+        int leftSum,rightSum;
+        if(currentElement > 0 && arr[currentElement - 1]){
+            leftSum += arr[currentElement - 1];            
+        }
+    }
+}
